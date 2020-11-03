95871c6373ec093ac9fda69f97bef45a3f5cdbcd
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-11-02 22:43:07 -0500

    commit tree dc315933b890f963944a933671cde708fe70b9b0

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,19 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    end_with?('.')
   end
 
   def question?
-
+    end_with?('?')
   end
 
   def exclamation?
-
+    end_with?('!')
   end
 
   def count_sentences
-
+    split(/(\.+|\?|!+)\s/).select { |word| word.start_with?(/\w/) }.count
   end
 end
